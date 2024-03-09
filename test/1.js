b65b97503d9c85c996675609930d620b1b398338
ayala revivo <ar0527167617@gmail.com>
2024-03-09 22:25:45 +0200

    Update 1.js

--- test/1.js
+++ test/1.js
@@ -1,3 +1,17 @@
 web.init();
 web.open('https://www.cibus-sodexo.co.il/');
 web.click("id=aAccount");
+web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
+web.click("//*[@class="cib-pink-grad cib-btn"]");
+
+
+
+web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+
+
+
+web.waitForExist("(//*[@class='cib-btn cib-pink-grad submit-order']");
+web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
+
+
