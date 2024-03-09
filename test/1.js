3b77c1b5edf98d4a2be8fb0c071f7ff9fccb7e5b
ayala revivo <ar0527167617@gmail.com>
2024-03-09 22:30:56 +0200

    Update 1.js

--- test/1.js
+++ test/1.js
@@ -1,17 +1,37 @@
 web.init();
 web.open('https://www.cibus-sodexo.co.il/');
 web.click("id=aAccount");
+// ההזמנות האחרונות שלי
 web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
-web.click("//*[@class="cib-pink-grad cib-btn"]");
+web.click("//*[@class='cib-pink-grad cib-btn']");
 
 
-
+//להזמין שוב
 web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 
 
-
-web.waitForExist("(//*[@class='cib-btn cib-pink-grad submit-order']");
+//אישור הזמנה
+web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
 web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
+
+//לוגו-דף הבית
+web.waitForExist("//*[@class='cibus-logo']");
+web.click("//*[@class='cibus-logo']");
+
+
+//ההזמנות האחרונות שלי
+web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
+web.click("//*[@class='cib-pink-grad cib-btn']");
+
+
+//להזמין שוב
+web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+
+
+//אישור הזמנה
+web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
+web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
