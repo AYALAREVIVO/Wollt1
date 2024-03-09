557e1aef046127a1a2522ea6d236664f330ba3a9
ayala revivo <ar0527167617@gmail.com>
2024-03-09 22:46:11 +0200

    Update 1.js

--- test/1.js
+++ test/1.js
@@ -47,3 +47,23 @@ web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
 web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
+
+//לוגו-דף הבית
+web.waitForExist("//*[@class='cibus-logo']");
+web.click("//*[@class='cibus-logo']");
+
+
+//ההזמנות האחרונות שלי
+web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
+web.click("//*[@class='cib-pink-grad cib-btn']");
+
+
+//להזמין שוב
+web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+
+
+//אישור הזמנה
+web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
+web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
+
