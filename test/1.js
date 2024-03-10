239665b2c17da6a34524848a691db2e1edde9b6c
ayala revivo <ar0527167617@gmail.com>
2024-03-10 22:15:12 +0200

    Update 1.js

--- test/1.js
+++ test/1.js
@@ -2,7 +2,8 @@ web.init();
 web.open('https://www.cibus-sodexo.co.il/');
 web.click("id=aAccount");
 
-//שם משתמש וסיסמה
+
+web.transaction('שם משתמש וסיסמה')
 web.type("id=user","ayala.revivo@verisoft.co" );
 web.waitForExist("//*[@class='login-btn cib-pink-grad']");
 web.click("//*[@class='login-btn cib-pink-grad']");
@@ -12,58 +13,51 @@ web.type("id=password","Aa0527167617!" );
 web.waitForExist("//*[@class='login-btn cib-pink-grad']");
 web.click("//*[@class='login-btn cib-pink-grad']");
 
-
-// ההזמנות האחרונות שלי
+web.transaction('ההזמנות האחרונות שלי')
 web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
 web.click("//*[@class='cib-pink-grad cib-btn']");
 
-
-//להזמין שוב
+web.transaction('להזמין שוב')
 web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 
+web.transaction('אישור הזמנה')
+web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
+web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
-//אישור הזמנה
+web.transaction('לוגו-דף הבית')
+web.waitForExist("//*[@class='cibus-logo']");
+web.click("//*[@class='cibus-logo']");
+
+web.transaction('ההזמנות האחרונות שלי')
+web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
+web.click("//*[@class='cib-pink-grad cib-btn']");
+
+web.transaction('להזמין שוב')
+web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
+
+web.transaction('אישור הזמנה')
 web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
 web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
 
-//לוגו-דף הבית
+Thread.sleep(2000);
+
+
+web.transaction('לוגו-דף הבית')
 web.waitForExist("//*[@class='cibus-logo']");
 web.click("//*[@class='cibus-logo']");
 
-
-//ההזמנות האחרונות שלי
+web.transaction('ההזמנות האחרונות שלי')
 web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
 web.click("//*[@class='cib-pink-grad cib-btn']");
 
-
-//להזמין שוב
+web.transaction('להזמין שוב')
 web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
 
-
-//אישור הזמנה
-web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
-web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
-
-
-//לוגו-דף הבית
-web.waitForExist("//*[@class='cibus-logo']");
-web.click("//*[@class='cibus-logo']");
-
-
-//ההזמנות האחרונות שלי
-web.waitForExist("//*[@class='cib-pink-grad cib-btn']");
-web.click("//*[@class='cib-pink-grad cib-btn']");
-
-
-//להזמין שוב
-web.waitForExist("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
-web.click("(//*[@class='cib-btn cib-pink-grad ng-star-inserted'])[1]");
-
-
-//אישור הזמנה
+web.transaction('אישור הזמנה')
 web.waitForExist("//*[@class='cib-btn cib-pink-grad submit-order']");
 web.click("//*[@class='cib-btn cib-pink-grad submit-order']");
 
